cee28d32a8266af02cbfdd76ad66e027afe88f0a
Botmasher <languagejosh@hotmail.com>
2017-10-01 13:09:26 -0700

    fix: Destructure food from combined reducers to map to state correctly

--- src/components/App.js
+++ src/components/App.js
@@ -40,11 +40,7 @@ class App extends Component {
 	render() {
 		return (
 			<div>
-				<input
-					type='text'
-					ref={(input) => this.input=input}
-					placeholder="Monday's Breakfast"
-				/>
+				<input type='text' ref={(input) => this.input=input} placeholder="Monday's Breakfast" />
 				<button onClick={this.submitFood}>Submit</button>
 				<pre>
 					Breakfast for Monday: {this.props.calendar && this.props.calendar.monday && this.props.calendar.monday.breakfast}
@@ -55,7 +51,8 @@ class App extends Component {
 }
 
 // react-redux getting updates from the store in lesson 3.4
-function mapStateToProps(calendar) {
+// 	note that with 2 reducers, we're now getting calendar as well as food as an object!
+function mapStateToProps({calendar, food}) {
 	const dayOrder = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
 	
 	// recreate store calendar as an array to display in shape of a grid on the page
